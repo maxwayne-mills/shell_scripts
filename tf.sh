02f2a93d389b97f9f7096a48cd51dbc480900bf5
Clarence Mills <cmills@opensitesolutions.com>
2018-04-29 10:51:56 -0400

    added: clean function, case statements: destroy,clean,fmt,validate and options on how to use

--- tf.sh
+++ tf.sh
@@ -8,7 +8,6 @@ ansible_repo="https://github.com/maxwayne-mills/ansible.git"
 shell_script_repo="https://github.com/maxwayne-mills/shell_scripts.git"
 cloud_init_repo="https://github.com/maxwayne-mills/cloud_init.git"
 
-
 init(){
  echo ""
  echo "Initializing provider"
@@ -27,37 +26,70 @@ init(){
 clone(){
   if [ -d ansible ]; then
 	rm -rf ansible
-  	git clone -depth=1 $ansible_repo
+  	git clone --depth=1 $ansible_repo
+	echo ""
   else
-  	git clone -depth=1 $ansible_repo
+  	git clone --depth=1 $ansible_repo
+	echo ""
   fi
 
   if [ -d shell_scripts ]; then
 	rm -rf shell_scripts
 	git clone --depth=1 $shell_script_repo
+	echo ""
   else
 	git clone --depth=1 $shell_script_repo
+	echo ""
   fi
 
   if [ -d cloud_init ]; then
 	rm -rf cloud_init
-	git clone --depth= $cloud_init_repo
+	git clone --depth=1 $cloud_init_repo
+	echo ""
   else
-	git clone --depth= $cloud_init_repo
+	git clone --depth=1 $cloud_init_repo
+	echo ""
   fi
 }
+
+clean(){
+	if [ -d shell_scripts ]; then
+		rm -rf shell_scripts
+	fi
+	if [ -d ansible ]; then
+		rm -rf ansible
+		if [ -f inventory ];then
+			rm inventory
+		fi
+	fi
+	if [ -d cloud_init ]; then
+		rm -rf cloud_init
+	fi
+	if [ -f terraform.tfstate ];then
+		rm -rf terraform*
+		rm -rf .terraform
+	fi
+}
+
 plan(){
+ clone
  init
  $tf plan $cred 
 }
 
 apply(){
- init
  clone
- $tf apply
+ init
+ $tf apply -auto-approve $cred
 }
 
 case $1 in
+-i | init)
+	$tf init
+;;
+clone)
+	clone
+;;
 plan)
 	plan
 ;;
@@ -70,7 +102,23 @@ show)
 list)
 	$tf state list
 ;;
+destroy)
+	$tf destroy $cred
+	clean
+;;
+clean)
+	clean
+;;
+-f|fmt)
+	$tf fmt
+;;
+-v|validate)
+	$tf validate
+;;
 *)
-	echo "Nothing entered"
+	echo "$0 [options]"
+	echo ""
+	echo "Options are: clone | plan | apply| show| list | destroy | fmt | validata"
+	echo ""
 ;;
 esac
