4cd6e0784e94727d9055de8c8e2b24b3061651df
Clarence Mills <clarence.mills@gmail.com>
2018-05-01 16:34:28 -0400

    seperate out installation of seperate third party applications

--- linux_workstationo_setup.sh
+++ linux_workstationo_setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Exit if any errors
-set -e
 clear
+set -e
 
 echo updating and upgrading packages
 sudo apt-get -y update
@@ -23,39 +23,21 @@ sudo apt-get -y install tree
 sudo apt-get -y install rsync
 sudo apt-get -y install curl
 sudo apt-get -y install git
-sudo apt-get -y install python-pip
 sudo apt-get -y install screen
 sudo apt-get -y install golang
 
-# Liquid-prompt requirement
-sudo apt-get -y install acpi
-# Install liquid-prompt
-cd /tmp
-git clone https://github.com/nojhan/liquidprompt.git
-if [ -d ~/repositories ];then
-	cp -Rvp /tmp/liquidprompt ~/repositories
-	source ~/bin/liquidprompt/liquidprompt
-else
-	mkdir ~/repositories
-	cp -Rvp /tmp/liquidprompt ~/repositories
-fi
-# Set permissions
-sudo chown -R cmills:cmills ~
-
 # upgrade pip
-pip install --upgrade pip
+#pip install --upgrade pip
 
-# Install Ansible
-sudo pip install ansible
-
+# Creature compfort stuff
+# Install aliases
 cd /tmp
 git clone https://github.com/maxwayne-mills/workstation.git
 cd workstation/system-setup
-# Install aliases
 cp bash_aliases ~/.bash_aliases
-cp  gitconfig ~/.gitconfig
+cp gitconfig ~/.gitconfig
 
-# Creature compfort stuff
+# Move ubuntu toolbar to the bottom
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 
 cd /tmp
@@ -65,13 +47,16 @@ cd /tmp/scripts/shell
 cp show_wireless.sh ~/bin
 cp manage-vm.sh ~/bin
 cp manage_virt.sh ~/bin
-cp backup-to-usb-drives.sh ~/bin
+cp backup_to_usb_drives.sh ~/bin
+
+# Install Ansible
+./install_ansible.sh
 
 # Install Packer
-./packer_install.sh
+./install_packer.sh
 
 # Install Terraform
-./terraform_install.sh
+./install_terraform.sh
 
 # Install vagrant
 ./install_vagrant.sh
@@ -79,85 +64,17 @@ cp backup-to-usb-drives.sh ~/bin
 # Install Vault
 ./install_vault.sh
 
-# Install Atom
-cd /tmp
-echo "Installing Atom"
-curl -L https://atom.io/download/deb/ --progress-bar -o atom.deb
-sudo dpkg -i atom.deb
-rm atom.deb
+#Install Dropbox
+./install_dropbox.sh
 
-## This script is to download and install slack
-dir=$(pwd)
-dl="https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb"
-
-# Cleanup uncompleted downloads before exiting
-trap 'echo - ""; "     removing uncompleted download"; rm slack.deb' SIGINT SIGTERM SIGtSTP
-
-# Download slack for ubuntu
-clear
-echo "Downloading Slack from $dl"
-echo ""
-curl -o $dir/slack.deb $dl
+# Install atom
+./install_atom.sh
 
 # Install slack
-echo ""
-echo -n "Installing Slack within $dir, do you want to continue Y or N ... "
-read answer
-if [ $answer == "y" -o "Y" ]; then
-	sudo dpkg -i $dir/slack.deb
-else
-	echo "You chose No, exiting ..."
-	exit 0
-fi
-
-## Install dropbox
-# link: https://www.dropbox.com/install-linux
-arch=$(uname -a | awk 'BEGIN {fs=" "};{print $12}')
-if [ "$arch" = "x86_64" ];then
-	dropboxlink=https://www.dropbox.com/download?plat=lnx.x86_64
-	cd ~ && wget -O - $dropboxlink | tar xzvf -
-
-	echo "Starting dropbox"
-	~/.dropbox-dist/dropboxd &
-	curl -L  https://www.dropbox.com/download?dl=packages/dropbox.py --progress-bar -o dropbox.py
-	mv dropbox.py ~/bin
-	chmod +x ~/bin/dropbox.py
-else
-	droplink=https://www.dropbox.com/download?plat=lnx.x86_64
-	cd ~ && wget -O - $droplink | tar xzvf -
-
-	echo "Starting dropbox"
-	~/.dropbox-dist/dropboxd &
-	curl -L  https://www.dropbox.com/download?dl=packages/dropbox.py --progress-bar -o dropbox.py
-	mv dropbox.py ~/bin
-	chmod +x ~/bin/dropbox.py
-fi
-
-## update bash script
-if [ -f ~/.bashrc ]; then
-	cat <<EOF > ~/.bashrc
-		# Start dropbox if it's not running
-		app=$(which dropbox.py)
-		dropbox_status=$($app status)
-
-		if [ "$dropbox_status" == "Dropbox isn't running!" ];then
-			echo "Starting Dropbox ..."
-			$app start
-			$app status
-		else
-			echo "Dropbox running"
-		fi
-	EOF
-else
-	echo "Did not find bashrc, not updating"
-fi
+./install_slack.sh
 
 # Install passsword gorilla
 curl -L http://ftp.us.debian.org/debian/pool/main/p/password-gorilla/password-gorilla_1.5.3.7-1_all.deb --progress-bar -o password-gorilla.deb
 sudo dpkg -i password-gorilla.deb
 
-# Cleanup
-rm -rf /tmp/workstation
-rm -rf /tmp/liquidprompt
-rm -rf /tmp/lscript
 echo "Finished"
