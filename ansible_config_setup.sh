cf864cdedb0f069495c739bac633ec86c3f1a99d
Clarence Mills <clarence.mills@gmail.com>
2018-05-08 11:44:56 -0400

    check for vagrant file

--- ansible_config_setup.sh
+++ ansible_config_setup.sh
@@ -8,28 +8,32 @@ tf=$(which terraform)
 local_user=cmills
 remote_user=deployuser
 
-#Figure out the provider being used
-provider=$(cat terraform.tfstate | grep -i provider | sort -u | awk 'BEGIN { FS = ":" }; { print $2 }' | sed 's/"//g' |sed 's/ //g' | awk 'BEGIN { FS = "." }; {print $2}')
-echo "Configuring for inventory file for $provider"
-
-# Build ansible inventory file from terraform state information
-if [ "$provider" == "scaleway" ]; then
-    # Get the computed server name
-    srv_name=$(cat terraform.tfstate | grep -i name | grep -iv "Ubuntu" | sed 's/ //g' | sed 's/"//g' | sed 's/,//g' | awk 'BEGIN { FS = ":" };{print $2}')
-
-    # obtain computed server IP address
-    ip_addr=$($tf show | grep -iw "ip =" | awk 'BEGIN { FS = " = " }; { print $2}')
-
-    echo "$srv_name	ansible_connection=ssh ansible_user=root	ansible_host=$ip_addr" > inventory
-elif [ "$provider" == "digitalocean" ]; then
-    # Get the computed server name
-    srv_name=$($tf show | grep "digitalocean_droplet." | awk 'BEGIN { FS = "." };{ print $2 }' | sed 's/://g')
-    
-    #obtain computed server ip address
-    ip_addr=$($tf show | grep "ip_address " | awk 'BEGIN { FS = " = " }; { print $2}')
-    echo "$srv_name	ansible_connection=ssh ansible_user=root	ansible_host=$ip_addr" > inventory
+if [ -f Vagrantfile ]; then
+    echo "Configuring for vagant"
 else
-    echo "Supported Providers are : Scaleway, Digital OCean"
+    #Figure out the provider being used
+    provider=$(cat terraform.tfstate | grep -i provider | sort -u | awk 'BEGIN { FS = ":" }; { print $2 }' | sed 's/"//g' |sed 's/ //g' | awk 'BEGIN { FS = "." }; {print $2}')
+    echo "Configuring for inventory file for $provider"
+
+    # Build ansible inventory file from terraform state information
+    if [ "$provider" == "scaleway" ]; then
+        # Get the computed server name
+        srv_name=$(cat terraform.tfstate | grep -i name | grep -iv "Ubuntu" | sed 's/ //g' | sed 's/"//g' | sed 's/,//g' | awk 'BEGIN { FS = ":" };{print $2}')
+
+        # obtain computed server IP address
+        ip_addr=$($tf show | grep -iw "ip =" | awk 'BEGIN { FS = " = " }; { print $2}')
+
+        echo "$srv_name	ansible_connection=ssh ansible_user=root	ansible_host=$ip_addr" > inventory
+    elif [ "$provider" == "digitalocean" ]; then
+        # Get the computed server name
+        srv_name=$($tf show | grep "digitalocean_droplet." | awk 'BEGIN { FS = "." };{ print $2 }' | sed 's/://g')
+        
+        #obtain computed server ip address
+        ip_addr=$($tf show | grep "ip_address " | awk 'BEGIN { FS = " = " }; { print $2}')
+        echo "$srv_name	ansible_connection=ssh ansible_user=root	ansible_host=$ip_addr" > inventory
+    else
+        echo "Supported Providers are : Scaleway, Digital OCean"
+    fi
 fi
 
 # Create Ansible configuration file
