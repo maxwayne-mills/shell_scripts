635cde0880faf7c12697ca4c3731ef3772778e6c
Clarence Mills <clarence.mills@gmail.com>
2019-07-17 20:18:10 -0400

    removed

--- execute_roles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-play=$(which ansible-playbook)
-server=$1
-domain=$2
-git_repo=$3
-
-$play ~/github/ansible/playbooks/publish_website.yml -i ~/.ansible/inventory -e "server_name=$server domain_name=$domain repo_name=$git_repo" -vv
